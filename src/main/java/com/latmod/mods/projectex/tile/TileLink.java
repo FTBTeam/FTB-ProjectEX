9812b1b062f98ed6744c0c53ebbb733ef78b549a
LatvianModder <latvianmodder@gmail.com>
2019-01-26 15:47:16 +0200

    Fixed EMC Link NBT dupe bug, improved performance a bit

--- src/main/java/com/latmod/mods/projectex/tile/TileLink.java
+++ src/main/java/com/latmod/mods/projectex/tile/TileLink.java
@@ -3,6 +3,7 @@ package com.latmod.mods.projectex.tile;
 import moze_intel.projecte.api.ProjectEAPI;
 import moze_intel.projecte.api.capabilities.IKnowledgeProvider;
 import moze_intel.projecte.config.ProjectEConfig;
+import moze_intel.projecte.utils.NBTWhitelist;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
@@ -23,6 +24,7 @@ public class TileLink extends TileEntity implements IItemHandler
 	public UUID owner = null;
 	public String name = "";
 	public ItemStack output = ItemStack.EMPTY;
+	private IKnowledgeProvider knowledgeProvider;
 
 	@Override
 	public void readFromNBT(NBTTagCompound nbt)
@@ -35,6 +37,8 @@ public class TileLink extends TileEntity implements IItemHandler
 		{
 			output = ItemStack.EMPTY;
 		}
+
+		knowledgeProvider = null;
 
 		super.readFromNBT(nbt);
 	}
@@ -105,13 +109,17 @@ public class TileLink extends TileEntity implements IItemHandler
 		{
 			if (!simulate)
 			{
-				IKnowledgeProvider provider = ProjectEAPI.getTransmutationProxy().getKnowledgeProviderFor(owner);
-				provider.setEmc(provider.getEmc() + (long) ((double) stack.getCount() * (double) value * ProjectEConfig.difficulty.covalenceLoss));
+				if (knowledgeProvider == null)
+				{
+					knowledgeProvider = ProjectEAPI.getTransmutationProxy().getKnowledgeProviderFor(owner);
+				}
+
+				knowledgeProvider.setEmc(knowledgeProvider.getEmc() + (long) ((double) stack.getCount() * (double) value * ProjectEConfig.difficulty.covalenceLoss));
 				EntityPlayerMP player = world.getMinecraftServer().getPlayerList().getPlayerByUUID(owner);
 
 				if (player != null)
 				{
-					provider.sync(player);
+					knowledgeProvider.sync(player);
 				}
 			}
 
@@ -119,6 +127,15 @@ public class TileLink extends TileEntity implements IItemHandler
 		}
 
 		return stack;
+	}
+
+	@Override
+	public void markDirty()
+	{
+		if (world != null)
+		{
+			world.markChunkDirty(pos, this);
+		}
 	}
 
 	@Override
@@ -137,24 +154,32 @@ public class TileLink extends TileEntity implements IItemHandler
 
 			if (value > 0L)
 			{
-				IKnowledgeProvider provider = ProjectEAPI.getTransmutationProxy().getKnowledgeProviderFor(owner);
+				if (knowledgeProvider == null)
+				{
+					knowledgeProvider = ProjectEAPI.getTransmutationProxy().getKnowledgeProviderFor(owner);
+				}
 
-				if (provider.getEmc() < value)
+				if (knowledgeProvider.getEmc() < value)
 				{
 					return ItemStack.EMPTY;
 				}
 
 				ItemStack stack = output.copy();
-				stack.setCount((int) (Math.min(amount, Math.min(output.getMaxStackSize(), provider.getEmc() / (double) value))));
+				stack.setCount((int) (Math.min(amount, Math.min(output.getMaxStackSize(), knowledgeProvider.getEmc() / (double) value))));
+
+				if (stack.hasTagCompound() && !NBTWhitelist.shouldDupeWithNBT(stack))
+				{
+					stack.setTagCompound(new NBTTagCompound());
+				}
 
 				if (!simulate)
 				{
-					provider.setEmc(provider.getEmc() - value * stack.getCount());
+					knowledgeProvider.setEmc(knowledgeProvider.getEmc() - value * stack.getCount());
 					EntityPlayerMP player = world.getMinecraftServer().getPlayerList().getPlayerByUUID(owner);
 
 					if (player != null)
 					{
-						provider.sync(player);
+						knowledgeProvider.sync(player);
 					}
 				}
 
